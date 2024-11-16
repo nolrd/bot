78a986a95d76f1e6c1ad642ee51a94fe8dc97f6f
nolrd <repito34@yandex.ru>
2024-11-16 23:02:07 +0500

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Changes to be committed:
    	modified:   3kurs.js
    
     Changes not staged for commit:
    	modified:   .env
    	modified:   3kurs.js
    	modified:   4kurs.js
    	modified:   bot.js
    	deleted:    files/malikpidor.txt
    	modified:   node_modules/.package-lock.json
    	modified:   package-lock.json
    	modified:   package.json
    
     Untracked files:
    	files/1.txt
    	files/10.txt
    	files/11.txt
    	files/12.txt
    	files/2.txt
    	files/3.txt
    	files/4.txt
    	files/5.txt
    	files/6.txt
    	files/7.txt
    	files/8.txt
    	files/9.txt
    	node_modules/crypto/

--- 3kurs.js
+++ 3kurs.js
@@ -5,7 +5,7 @@ module.exports = (bot) => {
     // Обработка выбора 3 курса
     bot.hears('3 курс', (ctx) => {
         ctx.reply(
-            'Добро пожаловать на 3 курс!',
+            'Добро пожаловать на 3 курс! Чтобы получить задание, необходимо оплатить.',
             Markup.keyboard([['Выбери задание']]) // Кнопка для выбора задания
                 .resize() // Подгоняет клавиатуру под экран
         );
@@ -22,18 +22,33 @@ module.exports = (bot) => {
 
     // Обработка нажатия на задание "1. Лох"
     bot.hears('1. Лох', (ctx) => {
-        const filePath = path.join(__dirname, 'files', 'malikpidor.txt'); // Путь к файлу в папке "files"
+        // Проверяем, что пользователь оплатил. Для простоты будем использовать переменную.
+        const paymentConfirmed = false; // Здесь должно быть условие, проверяющее успешную оплату
 
-        ctx.reply(
-            'Вы выбрали задание "Лох". Вот файл с заданием:',
-            {
-                reply_markup: {
-                    remove_keyboard: true, // Убираем клавиатуру
+        if (!paymentConfirmed) {
+            // Генерация ссылки для оплаты через ЮMoney
+            const paymentUrl = 'https://yoomoney.ru/quickpay/confirm?receiver=4100117637877905&quickpay-form=shop&targets=Sponsor%20this%20project&paymentType=AC&sum=15&label=e7db8012-53ee-4a1a-afa6-b448232116e7';
+
+            ctx.reply(
+                'Для того чтобы получить задание, необходимо сначала оплатить. Пожалуйста, перейдите по следующей ссылке для оплаты через ЮMoney:',
+                Markup.inlineKeyboard([
+                    [Markup.button.url('Оплатить через ЮMoney', paymentUrl)] // Кнопка с ссылкой на оплату
+                ])
+            );
+        } else {
+            const filePath = path.join(__dirname, 'files', 'malikpidor.txt'); // Путь к файлу в папке "files"
+
+            ctx.reply(
+                'Вы успешно оплатили. Вот файл с заданием:',
+                {
+                    reply_markup: {
+                        remove_keyboard: true, // Убираем клавиатуру
+                    }
                 }
-            }
-        );
+            );
 
-        // Отправляем файл
-        ctx.replyWithDocument({ source: filePath });
+            // Отправляем файл
+            ctx.replyWithDocument({ source: filePath });
+        }
     });
 };
